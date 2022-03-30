03dd725d5d3c8ca15e156abace3193e8de635d46
JediRhymeTrix <ved914980@gmail.com>
2022-03-30 12:09:21 -0500

    chore: update SQL

--- pygraph.sql
+++ pygraph.sql
@@ -1,19 +1,18 @@
 DROP TABLE IF EXISTS cs44_graphsql_output;
 CREATE TABLE cs44_graphsql_output AS
-WITH RECURSIVE solution(v1, v2, v3, v4, t, d) AS (
-    SELECT i, j, j, j, j, 1 
-    FROM wikiVote 
+WITH RECURSIVE solution(v1, v2, v3, t, d) AS (
+    SELECT i, j, j, j, 1 
+    FROM cliqueTest 
     UNION ALL 
-    SELECT solution.v1, solution.v2, solution.v4, solution.t, wikiVote.j, solution.d+1 
+    SELECT solution.v1, solution.v3, solution.t, cliqueTest.j, solution.d+1 
     FROM solution 
-    JOIN wikiVote ON wikiVote.i = solution.t 
+    JOIN cliqueTest ON cliqueTest.i = solution.t 
     WHERE 
-    wikiVote.j in (SELECT j FROM wikiVote w WHERE w.i=v1) AND 
-    wikiVote.j in (SELECT j FROM wikiVote w WHERE w.i=v2) AND 
-    wikiVote.j in (SELECT j FROM wikiVote w WHERE w.i=v3) AND 
-    wikiVote.j in (SELECT j FROM wikiVote w WHERE w.i=v4) AND 
-    d<4-1) 
-SELECT v1, v2, v4, t 
+    cliqueTest.j in (SELECT j FROM cliqueTest w WHERE w.i=v1) AND 
+    cliqueTest.j in (SELECT j FROM cliqueTest w WHERE w.i=v2) AND 
+    cliqueTest.j in (SELECT j FROM cliqueTest w WHERE w.i=v3) AND 
+    d<3-1) 
+SELECT v1, v3, t 
 FROM solution 
-WHERE v1 < v2 AND v2 < v4 AND v4<t
-ORDER BY v1, v2, v4, t;
+WHERE v1 < v3 AND v3<t
+ORDER BY v1, v3, t;
